d9e64f7139c775925db687cb50be879ecdbdfeac
Vita <nikaangel295@gmail.com>
2024-10-05 14:21:35 +0100

    add

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -10,16 +10,3 @@ export const loginUserSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().required(),
 });
-
-export const requestResetEmailSchema = Joi.object({
-  email: Joi.string().email().required(),
-});
-
-export const resetPasswordSchema = Joi.object({
-  password: Joi.string().required(),
-  token: Joi.string().required(),
-});
-
-export const loginWithGoogleOAuthSchema = Joi.object({
-  code: Joi.string().required(),
-});
